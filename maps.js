bfe7a329668a9db212ed0b170f65c4ab32282582
Pablo Aquino <pabloaquino1988@gmail.com>
2024-03-02 19:43:51 -0300

    style, add marker and distance

--- maps.js
+++ maps.js
@@ -1,43 +1,74 @@
-const url =
-  "https://nominatim.openstreetmap.org/search?q=Av.+18+de+Julio+1046,+Montevideo&format=json";
+const dist = document.querySelector("#distance");
+const form = document.querySelector("#addressForm");
+let map = null;
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const addressInput = document.querySelector("#address").value;
+  const encodedAddress = encodeURIComponent(addressInput).replace(/%20/g, "+");
 
-let lat, lon;
+  const url = `https://nominatim.openstreetmap.org/search?q=${encodedAddress}&format=json`;
 
-fetch(url)
-  .then((response) => {    
-    if (!response.ok) {
-      throw new Error("Ocurrió un error al obtener la respuesta.");
-    }   
-    return response.json();
-  })
-  .then((data) => {    
-    if (data.length > 0) {
-      const firstResult = data[0];     
-      console.log("Latitud:", firstResult.lat);
-      console.log("Longitud:", firstResult.lon);
-      lat = firstResult.lat;
-      lon = firstResult.lon;      
-      createMap(lat, lon);
-    } else {
-      console.log(
-        "No se encontraron resultados para la dirección proporcionada."
-      );
-    }
-  })
-  .catch((error) => {    
-    console.error("Ocurrió un error:", error);
-  });
+  let lat, lon;
 
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ocurrió un error al obtener la respuesta.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        const firstResult = data[0];
+        console.log("Latitud:", firstResult.lat);
+        console.log("Longitud:", firstResult.lon);
+        lat = firstResult.lat;
+        lon = firstResult.lon;
+        createMap(lat, lon);
+      } else {
+        console.log(
+          "No se encontraron resultados para la dirección proporcionada."
+        );
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+navigator.geolocation.getCurrentPosition((position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  createMap(lat, lon);
+});
 
 function createMap(lat, lon) {
-  let map = L.map("mi_mapa").setView([lat, lon], 18);
+  if (map) {
+    map.remove();
+  }
+  map = L.map("my_map").setView([lat, lon], 18);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker([lat, lon]).addTo(map)
-      .bindPopup('Ubicación encontrada.')
-      .openPopup();
+  const refPoint = L.marker([lat, lon]).addTo(map);
+  let destPoint;
+
+  function onMapClick(e) {
+    if (destPoint) {
+      map.removeLayer(destPoint);
+    }
+    destPoint = L.marker(e.latlng).addTo(map);
+    let distance = map.distance(refPoint.getLatLng(), destPoint.getLatLng());
+    let unitMesasure = "m";
+    if (distance > 1000) {
+      distance = distance / 1000;
+      unitMesasure = "km";
+    }
+    dist.textContent = `${distance.toFixed(1)} ${unitMesasure}.`;
+  }
+  map.on("click", onMapClick);
 }
